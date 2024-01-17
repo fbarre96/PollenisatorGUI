26e32c702193ba12fefb2354424f9c5d06c92c2e
fbarre96 <fbarre96@hotmail.fr>
2024-01-17 11:58:44 +0100

    add tkinter check

--- install.sh
+++ install.sh
@@ -24,6 +24,21 @@ install_packages() {
     esac
 }
 
+
+# Function to test tkinter installation
+test_tkinter () {
+    timeout 1 python -m tkinter 2>&1 | grep -q "No module named '_tkinter'"
+}
+
+
+# Function to install python with pyenv
+install_python_with_pyenv () {
+    local pythonversion
+    pythonversion=$(python3 --version | cut -d' ' -f2)
+    env PYTHON_CONFIGURE_OPTS="--enable-shared --with-tcltk-includes=/usr/include/tcl --with-tcltk-libs=\"/usr/lib/tcl8.6 /usr/lib/tk8.6\"" pyenv install "$pythonversion"
+    pyenv global "$pythonversion"
+}
+
 # Detect the operating system
 os_type=""
 if command_exists "apt-get"; then
@@ -50,4 +65,21 @@ fi
 echo "Installing your application using pipx..."
 pipx install . --force
 
-echo "Installation complete!"
+# Test tkinter installation
+if test_tkinter; then
+    if command_exists "pyenv"; then
+        install_python_with_pyenv
+        if test_tkinter; then
+            echo "Tkinter installation failed. Please install manually python with tcl-tk support and try again."
+        else
+            echo "Tkinter installation successful!"
+            echo "You can now run the application with the command pollenisator-gui"
+        fi
+    else
+        echo "Tkinter installation failed. Please install pyenv or install manually python with tcl-tk support and try again."
+    fi
+else
+    echo "Tkinter installation successful!"
+    echo "You can now run the application with the command pollenisator-gui"
+fi
+
